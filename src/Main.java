9fc6e943388f78c6751702576af27e6e10ea66f8
Nooomer <ampilogov.qaz@gmail.com>
2023-03-24 01:13:21 +0500

    New example

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,10 @@ public class Main {
         list.add("1dfdf");
         list.add("2dfdfsd");
         list.add("3fdfsdf");
-        //list.remove("fdf");
+        list.remove("fdf");
+       boolean res2 = list.contains("fdf");
+       boolean res3 = list.containsAll(List.of(new String[]{"123", "123", "123"}));
+        boolean res4 = list.containsAll(List.of(new String[]{"1dfdf", "2dfdfsd"}));
         //list.indexOf(2);
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
